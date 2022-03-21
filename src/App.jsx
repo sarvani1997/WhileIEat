275c872d8d6ce033765e8fb335584fc5817bde43
sarvani sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-03-21 15:50:47 +0530

    fetch movies list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function DiscoverMovies() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(2);
 
   useEffect(async () => {
     async function get() {
       try {
         const response = await axios.get(
-          'https://api.themoviedb.org/3/discover/movie?api_key=&sort_by=popularity.desc&page=1&with_watch_monetization_types=flatrate'
+          `${BASE_URL}/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=${page}&watch_region=IN&with_watch_monetization_types=flatrate`
         );
         console.log(response.data);
       } catch (error) {
@@ -16,9 +20,9 @@ function DiscoverMovies() {
       }
     }
     get();
-  }, []);
+  }, [page]);
 
-  return <div></div>;
+  return <div>data</div>;
 }
 
 export default DiscoverMovies;
