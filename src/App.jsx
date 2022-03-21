3fc78d9df3e4578aa0e03bf1e783f26077ef6033
sarvani sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-03-21 17:31:17 +0530

    render movies in grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
 
 const BASE_URL = import.meta.env.VITE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function DiscoverMovies() {
   const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
   const [laguages, setLanguages] = useState([]);
   const [genres, setGenres] = useState([]);
   const [watchProviders, setWatchProviders] = useState([]);
@@ -53,11 +54,20 @@ function DiscoverMovies() {
   useEffect(async () => {
     async function get() {
       try {
-        const response = await axios.get(
-          `${BASE_URL}/discover/movie?api_key=${API_KEY}&page=${page}&with_original_language=${language}&with_genres=${genre}&with_watch_providers=${watchProvider}&sort_by=popularity.desc&watch_region=IN&with_watch_monetization_types=flatrate`
-        );
-        setMovies(response.data);
-        console.log(response.data);
+        const response = await axios.get(`${BASE_URL}/discover/movie`, {
+          params: {
+            api_key: API_KEY,
+            page: page,
+            with_original_language: language,
+            with_genres: genre,
+            with_watch_providers: watchProvider,
+            sort_by: 'popularity.desc',
+            watch_region: 'IN',
+            with_watch_monetization_types: 'flatrate',
+          },
+        });
+        setMovies(response.data.results);
+        // console.log(response.data);
       } catch (error) {
         console.error(error);
       }
@@ -65,7 +75,28 @@ function DiscoverMovies() {
     get();
   }, [page, language, genre, watchProvider]);
 
-  return <div>data</div>;
+  console.log(movies);
+
+  return (
+    <div className="container">
+      <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+        {movies.map((movie, i) => {
+          return (
+            <div key={i} className="col">
+              <button className="thumbnail">
+                <img
+                  src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                  className="img-thumbnail"
+                  alt="..."
+                />
+                <p>{movie.title}</p>
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default DiscoverMovies;
