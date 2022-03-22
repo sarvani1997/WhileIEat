63b8b6a06a083632f039ba56458d718e6714f63e
sarvani sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-03-22 14:39:58 +0530

    filters

--- src/App.jsx
+++ src/App.jsx
@@ -4,116 +4,10 @@ import { Dialog } from '@reach/dialog';
 import '@reach/dialog/styles.css';
 import './App.css';
 import Pagination from './components/Pagination';
+import Filters from './components/Filters';
 
 const BASE_URL = import.meta.env.VITE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
-
-function Filters({ close, filters, setFilters, setPage }) {
-  const [laguages, setLanguages] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const [watchProviders, setWatchProviders] = useState([]);
-
-  useEffect(async () => {
-    async function get() {
-      try {
-        const laguages = await axios.get(
-          `${BASE_URL}/configuration/languages?api_key=${API_KEY}`
-        );
-        const genres = await axios.get(
-          `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`
-        );
-        const watchProviders = await axios.get(
-          `${BASE_URL}/watch/providers/movie?api_key=${API_KEY}`
-        );
-        setLanguages(laguages.data);
-        setGenres(genres.data.genres);
-        setWatchProviders(watchProviders.data.results);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    get();
-  }, []);
-
-  return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Language
-        </label>
-        <select
-          className="form-select"
-          aria-label="Default select example"
-          value={filters.language}
-          onChange={(e) => setFilters({ ...filters, language: e.target.value })}
-        >
-          <option value="">None</option>
-          {laguages.map((lag) => {
-            return (
-              <option key={lag.iso_639_1} value={lag.iso_639_1}>
-                {lag.english_name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Genre
-        </label>
-        <select
-          className="form-select"
-          aria-label="Default select example"
-          value={filters.genre}
-          onChange={(e) => setFilters({ ...filters, genre: e.target.value })}
-        >
-          <option value="">None</option>
-          {genres.map((g) => {
-            return (
-              <option key={g.id} value={g.id}>
-                {g.name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Watch Provider
-        </label>
-        <select
-          className="form-select"
-          aria-label="Default select example"
-          value={filters.watchProvider}
-          onChange={(e) =>
-            setFilters({ ...filters, watchProvider: e.target.value })
-          }
-        >
-          <option value="">None</option>
-          {watchProviders.map((w) => {
-            return (
-              <option key={w.provider_id} value={w.provider_id}>
-                {w.provider_name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <button
-        className="btn btn-primary m-2"
-        onClick={() =>
-          setFilters({ language: '', genre: '', watchProvider: '' })
-        }
-      >
-        Reset
-      </button>
-      <button className="btn btn-danger m-2" onClick={close}>
-        Save/Close
-      </button>
-    </form>
-  );
-}
 
 export default function DiscoverMovies() {
   const [movies, setMovies] = useState([]);
@@ -130,12 +24,6 @@ export default function DiscoverMovies() {
   });
   const [showDialog, setShowDialog] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
-
-  const open = () => {
-    setShowDialog(true);
-    setPage(1);
-  };
-  const close = () => setShowDialog(false);
 
   useEffect(async () => {
     async function get() {
@@ -182,6 +70,12 @@ export default function DiscoverMovies() {
     }
     get();
   }, [searchPage, searchValue]);
+
+  const open = () => {
+    setShowDialog(true);
+    setPage(1);
+  };
+  const close = () => setShowDialog(false);
 
   const onSearch = (e) => {
     e.preventDefault();
